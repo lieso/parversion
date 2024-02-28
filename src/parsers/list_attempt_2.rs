758c242800545f2f344e988ad7b44e777cc6b8cb
David Roussov <david.roussov@gmail.com>
2024-02-29 08:06:53 +1100

    define and return custom errors

--- src/parsers/list_attempt_2.rs
+++ src/parsers/list_attempt_2.rs
@@ -1,23 +1,30 @@
-use std::io::{Error, ErrorKind};
-use std::io::{self};
 use serde_json;
 
 use crate::utilities;
 use crate::models;
 use crate::prompts;
 
-pub async fn get_list_parser(document: &str) -> Result<Vec<models::list::ListParser>, io::Error> {
+pub enum Errors {
+    LlmRequestError,
+}
+
+pub async fn get_list_parser(document: &str) -> Result<Vec<models::list::ListParser>, Errors> {
     log::trace!("In get_list_parser");
 
     let mut parsers = Vec::new();
 
-    let llm_response = get_patterns(document).await.unwrap();
+
+
+    let llm_response = get_patterns(document).await?;
     println!("{:?}", llm_response);
+
+
+
 
     return Ok(parsers)
 }
 
-async fn get_patterns(document: &str) -> Result<serde_json::Value, io::Error> {
+async fn get_patterns(document: &str) -> Result<serde_json::Value, Errors> {
     log::trace!("In get_patterns");
 
     let prompt = format!("{} {}", prompts::list::patterns::LIST_GROUP_PROMPT, document);
@@ -26,11 +33,12 @@ async fn get_patterns(document: &str) -> Result<serde_json::Value, io::Error> {
 
     match maybe_llm_response {
         Ok(patterns) => {
-            return Ok(patterns)
+            log::debug!("Successfully obtained response from llm");
+            Ok(patterns)
         }
-        Err(_e) => {
-            log::debug!("Did not receive response from llm");
-            return Err(Error::new(ErrorKind::InvalidData, "error"));
+        Err(error) => {
+            log::error!("{}", error);
+            Err(Errors::LlmRequestError)
         }
     }
 }
