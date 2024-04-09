32b50c9cc1c525889575e3573d49201c01ee0ab1
David Roussov <david.roussov@gmail.com>
2024-04-09 12:39:59 +1000

    save llm responses to filesystem

--- src/utilities/llm.rs
+++ src/utilities/llm.rs
@@ -2,10 +2,35 @@ use std::env;
 use std::io::{Error, ErrorKind};
 use reqwest::header;
 use serde_json::json;
+use sha2::{Sha256, Digest};
+use std::collections::HashMap;
+use std::fs;
 use std::io::{self};
 
+const LLM_RESPONSE_STORE_PATH: &str = "src/data/llm_response_store";
+
 pub async fn get_llm_response(content: String) -> Result<serde_json::Value, io::Error> {
-    log::debug!("{}", content);
+    log::trace!("In get_llm_response");
+
+    let mut hasher = Sha256::new();
+    hasher.update(content.as_bytes());
+    let hash_result = hasher.finalize();
+    let hash = hash_result.iter()
+        .map(|byte| format!("{:02x}", byte))
+        .collect::<String>();
+    log::debug!("hash: {}", hash);
+
+    if cfg!(debug_assertions) {
+        log::info!("Running in debug mode");
+
+        let llm_response_store = get_llm_response_store().unwrap();
+
+        if let Some(response) = llm_response_store.get(&hash) {
+            return Ok(cleanup_llm_response(response));
+        }
+    } else {
+        log::info!("Running in release mode");
+    }
 
     if let Ok(openai_api_key) = env::var("OPENAI_API_KEY") {
         let request_json = json!({
@@ -44,29 +69,29 @@ pub async fn get_llm_response(content: String) -> Result<serde_json::Value, io::
                         };
 
                         let choice = &choices[0];
-                        log::debug!("{:?}", &choice);
+                        log::trace!("{:?}", &choice);
 
                         let message = &choice["message"];
-                        log::debug!("message: {:?}", message);
+                        log::trace!("message: {:?}", message);
 
                         let llm_response = &message["content"].as_str().unwrap();
-                        log::debug!("llm_response: {}", llm_response);
+                        log::trace!("llm_response: {}", llm_response);
 
-                        let llm_response = cleanup_llm_response(llm_response);
-                        log::debug!("fixed llm_response: {}", llm_response);
+                        if cfg!(debug_assertions) {
+                            log::info!("Saving LLM response to store");
 
-                        if let Ok(json) = serde_json::from_str(&llm_response) {
-                            Ok(json)
-                        } else {
-                            Ok(serde_json::Value::String(llm_response.to_string()))
+                            let mut llm_response_store = get_llm_response_store().unwrap();
+                            llm_response_store.insert(hash.to_string(), llm_response.to_string());
+
+                            save_llm_response_store(&llm_response_store).unwrap();
                         }
+
+                        Ok(cleanup_llm_response(llm_response))
                     }
                     Err(_err) => {
                         return Err(Error::new(ErrorKind::InvalidData, "error"));
                     }
                 }
-
-
             },
             Err(_err) => {
                 return Err(Error::new(ErrorKind::InvalidData, "error"));
@@ -78,16 +103,21 @@ pub async fn get_llm_response(content: String) -> Result<serde_json::Value, io::
     }
 }
 
-fn cleanup_llm_response(response: &str) -> &str {
+fn cleanup_llm_response(response: &str) -> serde_json::Value {
     log::trace!("In cleanup_llm_response");
 
     // remove code-block formatting
     let without_backticks = remove_codeblock_delimiters(response);
-    log::debug!("without_backticks: {}", without_backticks);
-    let without_label = remove_label_text(without_backticks);
-    log::debug!("without_label: {}", without_label);
+    log::trace!("without_backticks: {}", without_backticks);
 
-    return without_label;
+    let without_label = remove_label_text(without_backticks);
+    log::trace!("without_label: {}", without_label);
+
+    if let Ok(json) = serde_json::from_str(&without_label) {
+        return json;
+    } else {
+        return serde_json::Value::String(without_label.to_string());
+    }
 }
 
 fn remove_codeblock_delimiters(s: &str) -> &str {
@@ -104,4 +134,21 @@ fn remove_label_text(s: &str) -> &str {
     }
 
     s
+}
+
+fn get_llm_response_store() -> Result<HashMap<String, String>, serde_json::Error> {
+   match fs::read_to_string(&LLM_RESPONSE_STORE_PATH) {
+       Ok(contents) => {
+           serde_json::from_str(&contents)
+       },
+       Err(_) => {
+           Ok(HashMap::new())
+       }
+   }
+}
+
+fn save_llm_response_store(map: &HashMap<String, String>) -> Result<(), serde_json::Error> {
+    let json_contents = serde_json::to_string_pretty(map)?;
+    fs::write(LLM_RESPONSE_STORE_PATH, json_contents).unwrap();
+    Ok(())
 }
