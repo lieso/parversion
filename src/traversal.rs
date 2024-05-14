80c86d463011f700fda3eec983506917c28a06fd
David Roussov <david.roussov@gmail.com>
2024-05-15 09:48:12 +1000

    get complex types from output tree

--- src/traversal.rs
+++ src/traversal.rs
@@ -1,6 +1,7 @@
 use serde::{Serialize, Deserialize};
 use std::rc::{Rc};
 use std::collections::{HashMap, VecDeque};
+use uuid::Uuid;
 
 use crate::node::*;
 use crate::error::{Errors};
@@ -8,7 +9,8 @@ use crate::error::{Errors};
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct ComplexType {
     pub id: String,
-    pub values: HashMap<String, String>,
+    pub name: String,
+    pub fields: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -54,8 +56,8 @@ pub struct OutputMeta {
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Output {
-    pub complex_types: HashMap<String, Vec<ComplexType>>,
-    pub complex_objects: Vec<ComplexObject>,
+    pub complex_types: HashMap<String, ComplexType>,
+    pub complex_objects: HashMap<String, ComplexObject>,
     pub lists: HashMap<String, Vec<String>>,
     pub relationships: HashMap<String, Relationship>,
     pub meta: OutputMeta,
@@ -70,11 +72,8 @@ pub enum OutputFormats {
 
 const DEFAULT_OUTPUT_FORMAT: OutputFormats = OutputFormats::JSON;
 
-pub fn map_complex_object(basis_tree: Rc<Node>, output_tree: Rc<Node>) -> ComplexObject {
+pub fn map_complex_object(basis_tree: Rc<Node>, output_tree: Rc<Node>, complex_type: &ComplexType) -> ComplexObject {
     log::trace!("In map_complex_object");
-
-    let maybe_complex_type_name = basis_tree.complex_type_name.borrow();
-    let type_id_placeholder = maybe_complex_type_name.as_ref().unwrap();
 
     let mut values: HashMap<String, String> = HashMap::new();
 
@@ -100,7 +99,7 @@ pub fn map_complex_object(basis_tree: Rc<Node>, output_tree: Rc<Node>) -> Comple
 
     ComplexObject {
         id: output_tree.id.clone(),
-        type_id: type_id_placeholder.to_string(),
+        type_id: complex_type.id.to_string(),
         values: values,
     }
 }
@@ -150,13 +149,30 @@ impl Traversal {
 
             if let Some(basis_node) = search_tree_by_lineage(Rc::clone(&basis_tree), lineage.clone()) {
 
-                if basis_node.complex_type_name.borrow().is_some() {
-                    let complex_object = map_complex_object(basis_node, current.clone());
-                    log::debug!("complex_object: {:?}", complex_object);
+                if let Some(complex_type_name) = basis_node.complex_type_name.borrow().as_ref() {
 
-                    self.complex_objects.push(complex_object);
+                    let complex_type = self.complex_types.iter().find(|item| item.name == *complex_type_name);
+
+                    if let Some(complex_type) = complex_type {
+                        let complex_object = map_complex_object(basis_node.clone(), current.clone(), complex_type);
+                        self.complex_objects.push(complex_object);
+                    } else {
+                        let complex_type = ComplexType {
+                            id: Uuid::new_v4().to_string(),
+                            name: complex_type_name.to_string(),
+                            fields: basis_node.data.borrow().iter().map(|item| {
+                                item.name.to_string()
+                            }).collect()
+                        };
+                        self.type_count += 1;
+                        self.complex_types.push(complex_type.clone());
+
+                        let complex_object = map_complex_object(basis_node.clone(), current.clone(), &complex_type);
+                        self.complex_objects.push(complex_object);
+                    };
+
                     self.object_count += 1;
-                }
+                };
 
             } else {
                 log::warn!("Basis tree does to contain corresponding node to output tree!");
@@ -172,9 +188,9 @@ impl Traversal {
     }
 
     pub fn harvest(self) -> Result<String, Errors> {
-        let output = Output {
+        let mut output = Output {
             complex_types: HashMap::new(),
-            complex_objects: self.complex_objects.clone(),
+            complex_objects: HashMap::new(),
             lists: HashMap::new(),
             relationships: HashMap::new(),
             meta: OutputMeta {
@@ -182,6 +198,20 @@ impl Traversal {
                 type_count: self.type_count,
             }
         };
+
+        for complex_type in self.complex_types.iter() {
+            output.complex_types.insert(
+                complex_type.id.to_string(),
+                complex_type.clone()
+            );
+        }
+
+        for complex_object in self.complex_objects.iter() {
+            output.complex_objects.insert(
+                complex_object.id.to_string(),
+                complex_object.clone()
+            );
+        }
 
         let output_format = DEFAULT_OUTPUT_FORMAT;
         log::debug!("output_format: {:?}", output_format);
