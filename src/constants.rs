22d0dabc9944b3576b38f103634d363c6e561f83
David Roussov <david.roussov@protonmail.com>
2024-10-01 11:48:23 +1000

    use slices for constant definitions

--- src/constants.rs
+++ src/constants.rs
@@ -3,13 +3,13 @@ pub const TEXT_NODE_HASH: &str = "982d9e3eb996f559e633f4d194def3761d909f5a3b647d
 // echo -n "root" | sha256sum
 pub const ROOT_NODE_HASH: &str = "4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2";
 
-pub const UNSEEN_BLACKLISTED_ATTRIBUTES: [&str; 14] = [
+pub const UNSEEN_BLACKLISTED_ATTRIBUTES: &[&str] = &[
     "style", "bgcolor", "border", "cellpadding", "cellspacing",
     "width", "height", "rows", "cols", "wrap",
     "aria-hidden", "size", "op", "lang", "colspan"
 ];
 
-pub const UNSEEN_BLACKLISTED_ELEMENTS: [&str; 7] = [
+pub const UNSEEN_BLACKLISTED_ELEMENTS: &[&str] = &[
     "script", "meta", "link", "iframe", "svg", "style", "noscript"
 ];
 
