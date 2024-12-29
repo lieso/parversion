89d46f731ce438b6bf085ab6ed9e52f71e1c56e7
David Roussov <david.roussov@protonmail.com>
2024-12-29 12:52:14 +1100

    add fast hashes

--- src/hash.rs
+++ src/hash.rs
@@ -1,41 +1,71 @@
 use serde::{Serialize, Deserialize};
 use sha2::{Sha256, Digest};
-use std::fmt::Debug;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash as StdHash, Hasher as StdHasher};
 use std::fmt;
+use std::fmt::Debug;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Hash {
-    items: Vec<String>,
-    value: Option<String>,
+pub trait HashStrategy {
+    fn hash(data: &[u8]) -> String;
 }
 
-impl Hash {
+#[derive(Debug, Clone)]
+pub struct Sha256Strategy;
+impl HashStrategy for Sha256Strategy {
+    fn hash(data: &[u8]) -> String {
+        let mut hasher = Sha256::new();
+        hasher.update(data);
+        format!("{:x}", hasher.finalize())
+    }
+}
+
+// DefaultHasher (non-cryptographic) strategy
+#[derive(Debug, Clone)]
+pub struct DefaultHasherStrategy;
+impl HashStrategy for DefaultHasherStrategy {
+    fn hash(data: &[u8]) -> String {
+        let mut hasher = DefaultHasher::new();
+        data.hash(&mut hasher);
+        format!("{:x}", hasher.finish())
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Hasher<T: HashStrategy> {
+    items: Vec<String>,
+    value: Option<String>,
+    _strategy: std::marker::PhantomData<T>,
+}
+
+impl<T: HashStrategy> Hasher<T> {
     pub fn new() -> Self {
-        Hash {
+        Hasher {
             items: Vec::new(),
             value: None,
+            _strategy: std::marker::PhantomData,
         }
     }
 
     pub fn from_str(s: &str) -> Self {
-        Hash {
+        Hasher {
             items: Vec::new(),
-            value: Some(Self::hash_str(s)),
+            value: Some(T::hash(s.as_bytes())),
+            _strategy: std::marker::PhantomData,
         }
     }
 
-    pub fn from_items<T: ToString>(items: Vec<T>) -> Self {
-        let string_items = items.into_iter().map(|item|
-item.to_string()).collect::<Vec<String>>();
-        Hash {
+    pub fn from_items<U: ToString>(items: Vec<U>) -> Self {
+        let string_items = items.into_iter().map(|item| item.to_string()).collect();
+        Hasher {
             items: string_items,
             value: None,
+            _strategy: std::marker::PhantomData,
         }
     }
 
-    pub fn push<T: ToString>(&mut self, item: T) -> &mut Self {
+    pub fn push<U: ToString>(&mut self, item: U) -> &mut Self {
         self.items.push(item.to_string());
-        self.value = None; // Invalidate previous hash
+        self.value = None;
         self
     }
 
@@ -45,12 +75,8 @@ item.to_string()).collect::<Vec<String>>();
     }
 
     pub fn finalize(&mut self) -> &mut Self {
-        let mut hasher = Sha256::new();
-        let concatenated = self.items.join("");
-        hasher.update(concatenated);
-
-        let finalized = format!("{:x}", hasher.finalize());
-        self.value = Some(finalized);
+        let concatenated = self.items.join("").into_bytes();
+        self.value = Some(T::hash(&concatenated));
         self
     }
 
@@ -61,29 +87,17 @@ item.to_string()).collect::<Vec<String>>();
     pub fn to_string(&self) -> Option<String> {
         self.value.clone()
     }
-
-    pub fn hash_str(s: &str) -> String {
-        let mut hasher = Sha256::new();
-        hasher.update(s);
-        format!("{:x}", hasher.finalize())
-    }
 }
 
-impl PartialEq for Hash {
+impl<T: HashStrategy> PartialEq for Hasher<T> {
     fn eq(&self, other: &Self) -> bool {
-        if let Some(value) = &self.value {
-            if let Some(other_value) = &other.value {
-                return *value == *other_value;
-            }
-        }
-
-        false
+        self.value == other.value
     }
 }
 
-impl Eq for Hash {}
+impl<T: HashStrategy> Eq for Hasher<T> {}
 
-impl fmt::Display for Hash {
+impl<T: HashStrategy> fmt::Display for Hasher<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match &self.value {
             Some(value) => write!(f, "{}", value),
@@ -91,4 +105,7 @@ impl fmt::Display for Hash {
         }
     }
 }
+
+pub type Hash = Hasher<Sha256Strategy>;
+pub type FastHash = Hasher<DefaultHasherStrategy>;
 
