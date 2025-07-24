9bf99d54f9bd4bcc88dce0456c3f3f9eaf1387c1
David Roussov <david.roussov@gmail.com>
2025-07-25 08:48:22 +1000

    add PathSegment enum and new api for paths

--- src/path.rs
+++ src/path.rs
@@ -1,8 +1,15 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum PathSegment {
+    Key(String),
+    Index(usize),
+    AnyIndex,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Path {
-    pub segments: Vec<String>,
+    pub segments: Vec<PathSegment>,
 }
 
 impl Path {
@@ -14,17 +21,35 @@ impl Path {
 
     pub fn from_str(val: &str) -> Self {
         Path {
-            segments: vec![val.to_string()]
+            segments: vec![PathSegment::Key(val.to_string())],
         }
+    }
+
+    pub fn with_key_segment(&self, key: String) -> Self {
+        let mut new_path = self.clone();
+        new_path.segments.push(PathSegment::Key(key));
+        new_path
+    }
+
+    pub fn with_index_segment(&self, index: usize) -> Self {
+        let mut new_path = self.clone();
+        new_path.segments.push(PathSegment::Index(index));
+        new_path
+    }
+
+    pub fn with_index_segment_increment(&self, index: usize) -> Self {
+        let mut new_path = self.clone();
+        new_path.segments.push(PathSegment::Index(index + 1));
+        new_path
+    }
+
+    pub fn with_any_index_segment(&self) -> Self {
+        let mut new_path = self.clone();
+        new_path.segments.push(PathSegment::AnyIndex);
+        new_path
     }
 
     pub fn from_json_path(json_path: &String) -> Self {
         Self::new()
-    }
-
-    pub fn with_segment(&self, segment: String) -> Self {
-        let mut new_path = self.clone();
-        new_path.segments.push(segment);
-        new_path
     }
 }
