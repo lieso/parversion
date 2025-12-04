c365f3a1956c97c2adfd83bd7b28a507fc1e229c
David Roussov <david.roussov@gmail.com>
2025-12-04 22:35:55 +1100

    normalize parameters and variables

--- src/reduction.rs
+++ src/reduction.rs
@@ -5,10 +5,11 @@ use swc_ecma_parser::{Syntax, Parser, StringInput};
 use swc_ecma_parser::lexer::Lexer;
 use swc_ecma_ast::Program;
 use swc_ecma_ast::*;
-use swc_ecma_visit::{Visit, VisitWith};
+use swc_ecma_visit::{Visit, VisitMut, VisitMutWith, VisitWith};
 use swc_ecma_codegen::{Emitter, text_writer::JsWriter};
 use std::rc::Rc;
 use std::collections::HashMap;
+use swc_atoms::Atom;
 
 use crate::prelude::*;
 use crate::document::{Document, DocumentType};
@@ -121,73 +122,132 @@ struct AstExplorer {
 }
 
 impl Visit for AstExplorer {
-     fn visit_function(&mut self, f: &Function) {
-         self.fn_count += 1;
-
-         let func_decl = FnDecl {
-             ident: Ident::new("wrapped".into(), f.span, SyntaxContext::empty()),
-             declare: false,
-             function: Box::new(f.clone()),
-         };
-
-         let module = Module {
-             span: f.span,
-             body: vec![ModuleItem::Stmt(Stmt::Decl(Decl::Fn(func_decl)))],
-             shebang: None,
-         };
-
-         let mut buf = Vec::new();
-         {
-             let writer = JsWriter::new(self.cm.clone(), "\n", &mut buf, None);
-             let mut emitter = Emitter {
-                 cfg: Default::default(),
-                 comments: None,
-                 cm: self.cm.clone(),
-                 wr: Box::new(writer),
-             };
-
-             emitter.emit_module(&module).expect("emit failed");
-         }
-
-         let output = String::from_utf8(buf).expect("non-utf8 output from emitter");
+    fn visit_function(&mut self, f: &Function) {
+        self.fn_count += 1;
 
 
-         log::debug!("PRETTY FUNCTION:\n{}", output);
+
+        let mut cloned_fn = f.clone();
 
 
-         let hash = Hash::from_str(&output);
-         
-         log::debug!("hash: {}", hash.to_string().unwrap());
+
+        let mut normalizer = Normalizer::default();
+        cloned_fn.visit_mut_with(&mut normalizer);
 
 
-         *self.hash_count
-             .entry(hash.to_string().unwrap())
-             .or_insert(0) += 1;
 
 
-         f.visit_children_with(self);
-     }
 
-     fn visit_fn_decl(&mut self, n: &FnDecl) {
-         n.visit_children_with(self);
-     }
+        let func_decl = FnDecl {
+            ident: Ident::new("fn".into(), f.span, SyntaxContext::empty()),
+            declare: false,
+            function: Box::new(cloned_fn),
+        };
 
-     fn visit_var_decl(&mut self, n: &VarDecl) {
-         n.visit_children_with(self);
-     }
+        let module = Module {
+            span: f.span,
+            body: vec![ModuleItem::Stmt(Stmt::Decl(Decl::Fn(func_decl)))],
+            shebang: None,
+        };
 
-     fn visit_class_decl(&mut self, n: &ClassDecl) {
-         n.visit_children_with(self);
-     }
+        let mut buf = Vec::new();
+        {
+            let writer = JsWriter::new(self.cm.clone(), "\n", &mut buf, None);
+            let mut emitter = Emitter {
+                cfg: Default::default(),
+                comments: None,
+                cm: self.cm.clone(),
+                wr: Box::new(writer),
+            };
 
-     fn visit_expr(&mut self, n: &Expr) {
-         if let Expr::Call(call) = n {
-             call.visit_children_with(self);
-             return;
-         }
-         n.visit_children_with(self);
-     }
- }
+            emitter.emit_module(&module).expect("emit failed");
+        }
+
+        let output = String::from_utf8(buf).expect("non-utf8 output from emitter");
+
+
+        log::debug!("PRETTY FUNCTION:\n{}", output);
+
+
+        let hash = Hash::from_str(&output);
+        
+        log::debug!("hash: {}", hash.to_string().unwrap());
+
+
+        *self.hash_count
+            .entry(hash.to_string().unwrap())
+            .or_insert(0) += 1;
+
+
+        f.visit_children_with(self);
+    }
+
+    fn visit_fn_decl(&mut self, n: &FnDecl) {
+        n.visit_children_with(self);
+    }
+
+    fn visit_var_decl(&mut self, n: &VarDecl) {
+        n.visit_children_with(self);
+    }
+
+    fn visit_class_decl(&mut self, n: &ClassDecl) {
+        n.visit_children_with(self);
+    }
+
+    fn visit_expr(&mut self, n: &Expr) {
+        if let Expr::Call(call) = n {
+            call.visit_children_with(self);
+            return;
+        }
+        n.visit_children_with(self);
+    }
+}
+
+#[derive(Default)]
+struct Normalizer {
+    rename_map: HashMap<Atom, Atom>,
+}
+
+impl VisitMut for Normalizer {
+    fn visit_mut_function(&mut self, f: &mut Function) {
+
+        for (idx, param) in f.params.iter_mut().enumerate() {
+            if let Pat::Ident(bi) = &mut param.pat {
+                let old = bi.id.sym.clone();
+                let new: Atom = format!("p{}", idx).into();
+                bi.id.sym = new.clone();
+                self.rename_map.insert(old, new);
+            }
+        }
+
+        if let Some(body) = &mut f.body {
+            for stmt in &mut body.stmts {
+                if let Stmt::Decl(Decl::Var(var_decl)) = stmt {
+                    for (idx, decl) in var_decl.decls.iter_mut().enumerate() {
+                        if let Pat::Ident(bi) = &mut decl.name {
+                            let old = bi.id.sym.clone();
+                            let new: Atom = format!("v{}", idx).into();
+                            bi.id.sym = new.clone();
+                            self.rename_map.insert(old, new);
+                        }
+                    }
+                }
+            }
+
+            body.visit_mut_with(self);
+        }
+    }
+
+    fn visit_mut_ident(&mut self, i: &mut Ident) {
+        if let Some(new) = self.rename_map.get(&i.sym) {
+            i.sym = new.clone();
+        }
+    }
+}
+
+
+
+
 
 fn explore_with_visitor(program: &Program) {
      let cm: Lrc<SourceMap> = Default::default();
@@ -201,5 +261,6 @@ fn explore_with_visitor(program: &Program) {
      program.visit_with(&mut explorer);
      println!("fn count: {}", explorer.fn_count);
      println!("hash count: {:?}", explorer.hash_count);
+     println!("hash count: {}", explorer.hash_count.len());
  }
 
