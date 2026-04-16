dee00f0bae4a98a2a43f4c351dba9db1f03f70e4
David Roussov <david.roussov@gmail.com>
2026-04-17 09:53:31 +1000

    remove old traversal logic

--- src/document.rs
+++ src/document.rs
@@ -75,14 +75,7 @@ impl Document {
 
         let mut inner_schema: HashMap<String, SchemaNode> = HashMap::new();
 
-        let result = do_something(Arc::clone(&meta_context))?;
-
-        //let result = process_graph(
-        //    Arc::clone(&meta_context),
-        //    graph_root,
-        //    &mut inner_schema,
-        //    &classification.lineage,
-        //)?;
+        let result = process_graph(Arc::clone(&meta_context))?;
 
         let data = {
             match serde_json::to_string(&result) {
@@ -655,157 +648,8 @@ fn apply_schema_transformations_json(
 }
 
 fn process_graph(
-    meta_context: Arc<RwLock<MetaContext>>,
-    graph: Graph,
-    schema: &mut HashMap<String, SchemaNode>,
-    schema_lineage: &Lineage,
-) -> Result<Map<String, Value>, Errors> {
-
-    let mut result: Map<String, Value> = Map::new();
-    
-    fn recurse(
-        meta_context: Arc<RwLock<MetaContext>>,
-        graph_node: Graph,
-        result: &mut Map<String, Value>
-    ) {
-        let contexts = {
-            let lock = read_lock!(meta_context);
-            lock.contexts.clone().unwrap()
-        };
-
-        for child in &read_lock!(graph_node).children {
-            let subgraph_hash: Hash = read_lock!(child).subgraph_hash.clone();
-            let basis_network: Arc<BasisNetwork> = {
-                let lock = read_lock!(meta_context);
-                lock.get_basis_network_by_lineage_and_subgraph_hash(&subgraph_hash).unwrap().expect("Could not find basis network")
-            };
-
-            match &basis_network.transformation {
-                NetworkType::Degenerate => {
-                    recurse(
-                        Arc::clone(&meta_context),
-                        Arc::clone(&child),
-                        result,
-                    );
-                },
-                NetworkType::Complex(transformation) => {
-                    let mut inner_result: Map<String, Value> = Map::new();
-
-                    recurse(
-                        Arc::clone(&meta_context),
-                        Arc::clone(&child),
-                        &mut inner_result,
-                    );
-
-                    let inner_result_value = Value::Object(inner_result.clone());
-
-                    if let Some(existing_object) = result.get_mut(&transformation.image) {
-                        if let Value::Array(ref mut arr) = existing_object {
-                            arr.push(inner_result_value.clone());
-                        } else {
-                            *existing_object = json!(vec![
-                                existing_object.clone(),
-                                inner_result_value.clone()
-                            ]);
-                        }
-                    } else {
-                        result.insert(transformation.image.clone(), inner_result_value);
-                    }
-                }
-            }
-        }
-
-        let context = contexts.get(&read_lock!(graph_node).id).unwrap();
-        let data_node = &context.data_node;
-        let basis_node = {
-            let lock = read_lock!(meta_context);
-            lock.get_basis_node_by_lineage(&context.lineage).expect("Could not get basis node by lineage").unwrap()
-        };
-        let json_nodes: Vec<JsonNode> = basis_node
-.transformations
-            .clone()
-            .into_iter()
-            .map(|transformation| {
-                transformation
-                    .transform(Arc::clone(&data_node))
-                    .expect("Could not transform data node field")
-            })
-            .collect();
-
-        for json_node in json_nodes.into_iter() {
-            let json = json_node.json;
-            let value = json!(json.value.trim().to_string());
-            result.insert(json.key, value);
-        }
-    }
-
-    recurse(
-        Arc::clone(&meta_context),
-        Arc::clone(&graph),
-        &mut result
-    );
-
-    Ok(result)
-}
-
-fn process_node(
-    meta_context: Arc<RwLock<MetaContext>>,
-    context: Arc<Context>,
-    result: &mut HashMap<String, Value>,
-    schema: &mut HashMap<String, SchemaNode>,
-    schema_lineage: &Lineage,
-) -> Result<(), Errors> {
-    log::trace!("In process_node");
-
-    let maybe_basis_node = {
-        let lock = read_lock!(meta_context);
-        lock.get_basis_node_by_lineage(&context.lineage)
-            .expect("Could not get basis node by lineage")
-    };
-
-    if let Some(basis_node) = maybe_basis_node {
-        let json_nodes: Vec<JsonNode> = basis_node
-            .transformations
-            .clone()
-            .into_iter()
-            .map(|transformation| {
-                transformation
-                    .transform(Arc::clone(&context.data_node))
-                    .expect("Could not transform data node")
-            })
-            .collect();
-
-        for json_node in json_nodes.into_iter() {
-            let json = json_node.json;
-            let key = json.key.clone();
-            let trimmed_value = json!(json.value.trim().to_string());
-
-            let mut schema_node =
-                SchemaNode::new(&key, &json_node.description, schema_lineage, "string");
-
-            if let Some(existing_value) = result.get_mut(&schema_node.name) {
-                if let Value::Array(ref mut arr) = existing_value {
-                    arr.push(trimmed_value.clone());
-                } else {
-                    *existing_value = json!(vec![existing_value.clone(), trimmed_value.clone()]);
-                }
-
-                schema_node.data_type = "array".to_string();
-
-                schema.insert(schema_node.name.clone(), schema_node);
-            } else {
-                result.insert(schema_node.name.clone(), trimmed_value.clone());
-                schema.insert(schema_node.name.clone(), schema_node);
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn do_something(
     meta_context: Arc<RwLock<MetaContext>>
-) -> Result<String, Errors> {
+) -> Result<Map<String, Value>, Errors> {
 
     let basis_graph: BasisGraph = read_lock!(meta_context).basis_graph.clone().unwrap();
     let canonicalization: CanonicalizationTransformation = basis_graph.canonicalization;
@@ -963,16 +807,7 @@ fn do_something(
         }
     }
 
-
-    let foo = serde_json::to_string_pretty(&result).expect("Could not make a JSON string");
-
-    log::debug!("==================================================================================================================================================================================================================================================================================================================================================================================================================");
-    log::debug!("{}", foo);
-
-
-
-    unimplemented!();
-
+    Ok(result)
 }
 
 fn process_network(
