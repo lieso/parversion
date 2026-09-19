62a5885361f6eaa3fd4e02cbd57334785ad3f5c7
David Roussov <david.roussov@gmail.com>
2026-09-19 15:19:23 +1000

    init reflexive/comparative network relationships

--- src/reasoner/network_relationship.rs
+++ src/reasoner/network_relationship.rs
@@ -14,5 +14,35 @@ pub async fn network_relationship<R: Reasoner>(
     left: Arc<BasisNetwork>,
     right: Arc<BasisNetwork>
 ) -> Result<(NetworkRelationship, ReasonerMetadata), Errors> {
+    if left.id == right.id {
+        network_relationship_reflexive(
+            reasoner,
+            Arc::clone(&normalization_context),
+            left.clone()
+        ).await
+    } else {
+        network_relationship_comparative(
+            reasoner,
+            Arc::clone(&normalization_context),
+            left.clone(),
+            right.clone()
+        ).await
+    }
+}
+
+async fn network_relationship_reflexive<R: Reasoner>(
+    reasoner: &R,
+    normalization_context: Arc<RwLock<NormalizationContext>>,
+    network: Arc<BasisNetwork>,
+) -> Result<(NetworkRelationship, ReasonerMetadata), Errors> {
+    unimplemented!()
+}
+
+async fn network_relationship_comparative<R: Reasoner>(
+    reasoner: &R,
+    normalization_context: Arc<RwLock<NormalizationContext>>,
+    left: Arc<BasisNetwork>,
+    right: Arc<BasisNetwork>
+) -> Result<(NetworkRelationship, ReasonerMetadata), Errors> {
     unimplemented!()
 }
